1fabf1fbf2a427096dd6eb1df53af9cb1d72a9e6
Alex Diker <alex.diker@georgebrown.ca>
2017-03-27 15:31:19 -0400

    hello world
    portfolio :100:

---
+++ assets/js/showTime_alex.js
@@ -0,0 +1,24 @@
+/**
+ * Alex Diker
+ */
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function showTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('displayTime').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+        showTime()
+    }, 500);
+}
+showTime();
